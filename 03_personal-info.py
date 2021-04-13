139cbfb187d07e1c3c1e27fa34ee732aa54d4594
Wilowisk <66510101+Wilowisk@users.noreply.github.com>
2021-04-13 09:37:01 +0800

    Add files via upload

---
+++ 03_personal-info.py
@@ -0,0 +1,7 @@
+first_Name = input("First Name: ")
+last_Name = input("Last Name: ")
+location = input("Location: ")
+age = input("Age: ")
+
+print("\nYour name is " + first_Name + " " + last_Name + ", you are " + age +
+      " years old and you live near " + location + ".")
